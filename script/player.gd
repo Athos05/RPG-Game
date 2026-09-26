4aa788345ed757a3143198c13160c808f2665a56
Rmate20x <150480932+Rmate20x@users.noreply.github.com>
2026-09-26 12:26:29 +0200

    -

---
+++ script/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+@export var speed := 100.0
+
+func _physics_process(_delta):
+	var direction = Input.get_vector(
+		"ui_left",
+		"ui_right",
+		"ui_up",
+		"ui_down"
+	)
+
+	velocity = direction * speed
+	move_and_slide()
